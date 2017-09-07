e137fddc813deacc2e48a8e12baa2b00c7b1d835
Yan Levinsky <Legioncorehost@gmail.com>
2017-09-07 15:24:15 +0400

    donation service added, check is app rated added, fix .gitignore file

--- app/src/main/java/core/legion/mtglife/ui/MainActivity.java
+++ app/src/main/java/core/legion/mtglife/ui/MainActivity.java
@@ -1,8 +1,14 @@
 package core.legion.mtglife.ui;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -11,11 +17,16 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Toast;
 
+import com.anjlab.android.iab.v3.BillingProcessor;
+import com.anjlab.android.iab.v3.TransactionDetails;
+
 import core.legion.mtglife.R;
 
 import static trikita.anvil.DSL.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BillingProcessor billing;
 
     private int columns, count;
 
@@ -25,13 +36,49 @@ public class MainActivity extends AppCompatActivity {
     private PlayerAdapter adapter;
     private RecyclerView recyclerView;
 
+    private String DONATE_1;
+    private String DONATE_2;
+    private String DONATE_3;
+    private String DONATE_4;
+
+    private String LICENSE_KEY;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
 
+        DONATE_1 = getString(R.string.donate_1);
+        DONATE_2 = getString(R.string.donate_2);
+        DONATE_3 = getString(R.string.donate_3);
+        DONATE_4 = getString(R.string.donate_4);
+
+        LICENSE_KEY = getString(R.string.license_key);
+
+        checkForRate();
+
         initNavigationView();
         initRecyclerView();
+        initBilling();
+    }
+
+    private void checkForRate() {
+        SharedPreferences preferences = getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE);
+        if (!preferences.getBoolean("isRated", false)) {
+            new AlertDialog.Builder(this)
+                    .setTitle(R.string.txt_rate_title)
+                    .setMessage(R.string.txt_rate_message)
+                    .setPositiveButton(R.string.txt_rate_positive, (dialog, which) -> {
+                        Intent intent = new Intent(Intent.ACTION_VIEW);
+                        intent.setData(Uri.parse(getString(R.string.link_play_market)));
+                        startActivity(intent);
+                    })
+                    .setNegativeButton(R.string.txt_rate_negative, (dialog, which) -> {
+                    })
+                    .setNeutralButton(R.string.txt_rate_neutral, (dialog, which) ->
+                            preferences.edit().putBoolean("isRated", true).apply())
+                    .show();
+        }
     }
 
     @Override
@@ -45,6 +92,13 @@ public class MainActivity extends AppCompatActivity {
         if (drawerLayout.isDrawerOpen(GravityCompat.START))
             drawerLayout.closeDrawer(GravityCompat.START);
         else super.onBackPressed();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!billing.handleActivityResult(requestCode, resultCode, data)) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
     private void initNavigationView() {
@@ -62,10 +116,64 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.menu_flip_coin:
                     showFlipCoinDialog();
                     break;
+                case R.id.menu_purchase:
+                    showPurchaseDialog();
+                    break;
             }
             drawerLayout.closeDrawer(START);
             return true;
         });
+    }
+
+    private void showPurchaseDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Donate")
+                .setIcon(R.drawable.ic_coin)
+                .setItems(
+                        new String[]{"Thank you!", "Good job!", "Great!", "Awesome!"},
+                        (dialog, which) -> {
+                            switch (which) {
+                                case 0:
+                                    billing.purchase(MainActivity.this, DONATE_1);
+                                    break;
+                                case 1:
+                                    billing.purchase(MainActivity.this, DONATE_2);
+                                    break;
+                                case 2:
+                                    billing.purchase(MainActivity.this, DONATE_3);
+                                    break;
+                                case 3:
+                                    billing.purchase(MainActivity.this, DONATE_4);
+                                    break;
+                            }
+                        })
+                .show();
+    }
+
+    private void initBilling() {
+        billing = new BillingProcessor(this, LICENSE_KEY, new BillingProcessor.IBillingHandler() {
+            @Override
+            public void onProductPurchased(@NonNull String productId, @Nullable TransactionDetails details) {
+                Toast.makeText(MainActivity.this, "Purchased!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPurchaseHistoryRestored() {
+
+            }
+
+            @Override
+            public void onBillingError(int errorCode, @Nullable Throwable error) {
+                Toast.makeText(MainActivity.this, "Something wrong, try later", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onBillingInitialized() {
+
+            }
+        });
+        if (!BillingProcessor.isIabServiceAvailable(this))
+            Toast.makeText(this, "Purchase is not available", Toast.LENGTH_SHORT).show();
     }
 
     private void initRecyclerView() {
